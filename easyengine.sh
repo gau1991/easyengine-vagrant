c11aa715d90f9b1573315b0e9fb62ccd7361851e
gau1991 <gaurav.astikar@rtcamp.com>
2014-09-05 15:33:54 +0530

    Fixed some halt issue

--- easyengine.sh
+++ easyengine.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
+alias eeend="echo =========================================================================================================================="
 sudo echo -e "[user]\n\tname = EasyEngine\n\temail = root@easyengine.com" > ~/.gitconfig
+eeend
 wget -qO ee rt.cx/ee && sudo bash ee stable
+eeend
 sudo ee stack install
+eeend
 sudo ee site create easyengine.com --wpfc
+eeend
 sudo halt
